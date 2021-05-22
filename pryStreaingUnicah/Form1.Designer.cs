b7bb7be8afe8c2dee24805625ac4936747d5f135
ernestojhn <57277336+ernestojhn@users.noreply.github.com>
2021-05-22 13:30:49 -0600

    Add files via upload

---
+++ pryStreaingUnicah/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace pryStreaingUnicah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtDescTipoPelicula = new System.Windows.Forms.TextBox();
+            this.chkEstado = new System.Windows.Forms.CheckBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(54, 74);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(154, 13);
+            this.lblDesc.TabIndex = 0;
+            this.lblDesc.Text = "Descripcion del tipo de pelicula";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(54, 114);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(40, 13);
+            this.lblEstado.TabIndex = 1;
+            this.lblEstado.Text = "Estado";
+            // 
+            // txtDescTipoPelicula
+            // 
+            this.txtDescTipoPelicula.Location = new System.Drawing.Point(214, 71);
+            this.txtDescTipoPelicula.Name = "txtDescTipoPelicula";
+            this.txtDescTipoPelicula.Size = new System.Drawing.Size(142, 20);
+            this.txtDescTipoPelicula.TabIndex = 2;
+            // 
+            // chkEstado
+            // 
+            this.chkEstado.AutoSize = true;
+            this.chkEstado.Location = new System.Drawing.Point(214, 114);
+            this.chkEstado.Name = "chkEstado";
+            this.chkEstado.Size = new System.Drawing.Size(142, 17);
+            this.chkEstado.TabIndex = 3;
+            this.chkEstado.Text = "Habilitado/Deshabilitado";
+            this.chkEstado.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(132, 185);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(281, 185);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 297);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.chkEstado);
+            this.Controls.Add(this.txtDescTipoPelicula);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblDesc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtDescTipoPelicula;
+        private System.Windows.Forms.CheckBox chkEstado;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ pryStreaingUnicah/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryStreaingUnicah
+{
+    public partial class Form1 : Form
+    {
+        StreamingUnicahEntities entityStreaming = new StreamingUnicahEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtDescTipoPelicula.Text.Equals("")) {
+                MessageBox.Show("Por favor ingresar descripcion");
+                return;
+            }
+
+            TiposPeliculas tbTipoPelicula = new TiposPeliculas();
+            tbTipoPelicula.DescripcionTipoPelicula = txtDescTipoPelicula.Text;
+            tbTipoPelicula.EstadoTipoPelicula = chkEstado.Checked;
+            entityStreaming.TiposPeliculas.Add(tbTipoPelicula);
+
+            entityStreaming.SaveChanges();//se confirma la insercion
+
+            txtDescTipoPelicula.Text = "";
+            chkEstado.Checked = false;
+            
+        }
+    }
+}
